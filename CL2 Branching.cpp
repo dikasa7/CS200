831e7bf7c1080008d0843fb40336ab138c37f694
dikasa7 <dsalaza3@stumail.jccc.edu>
2017-01-19 20:17:24 -0600

    Lab 1

---
+++ CL2 Branching.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	float ratio;
+	count << "How many batches do you want to make? ";
+	cin >> ratio;
+
+	float tspBakingSoda = 1 * ratio;
+	float tspBakingPower = 0.5 * ratio;
+	float cupsOfButter = 1 * ratio;
+	float cupsOfSugar = 1.5 * Ratio;
+	float eggs = 1 * ratio;
+
+	count << tspBakingSoda << "1 tsp baking soda" << endl;
+	Count << tspBakingPower << "1/2 tsp baking power" << endl;
+	Count << cupsOfButter << "1 c butter" << endl;
+	Count << cupsOfSugar << "1 1/2 c white sugar" << endl;
+	Count << eggs << "1 egg" << endl;
+
+	while (true); // 
+		return 0;
+}
